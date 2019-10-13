3980760a3aa924149329662d774626f10a4fd84a
Ella <ellazhang37@gmail.com>
2019-10-13 09:25:42 -0400

    + Update Guitar Player

--- TeamPlayer/GamePlayer/src/bus/GuitarGame.java
+++ TeamPlayer/GamePlayer/src/bus/GuitarGame.java
@@ -1,7 +1,17 @@
 package bus;
 
 import java.util.Scanner;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.swing.JOptionPane;
+
 import java.util.Random;
+
+import java.io.File;
 
 public class GuitarGame extends GuitarPlayer {
 	private int m1;
@@ -13,86 +23,119 @@ public class GuitarGame extends GuitarPlayer {
 	private Color color;
 	private GuitarBrand brand;
 
-	private String m1Location;
-	private String m2Location;
-	
+	// private static String m1Location = "D:/TeamPlayer/TeamPlayer/GamePlayer/Musics/SecretBase.wav";
+	// private static String m2Location = "D:/TeamPlayer/TeamPlayer/GamePlayer/Musics/MiracleMountain.wav";
+
 	public long getId() {
 		return this.id;
 	}
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public int getMI() {
 		return this.m1;
 	}
-	
+
 	public int getMII() {
 		return this.m2;
 	}
-	
+
 	public GuitarBrand getBrand() {
 		return this.brand;
 	}
-	
+
 	public Color getColor() {
 		return this.color;
 	}
-	
-	
+
 	public GuitarGame() {
-		
+
 	}
-	
+
 	public void start() {
 		System.out.println("You choosed Guitar Player");
-		
-		while(true){
-		     System.out.println("Please input your ID (numbers only): ");
-		 try {
-		     id = Long.parseLong(inputScanner.nextLine());
-		     break;
-		 } catch (NumberFormatException e){
-		     System.out.println("Your input is invalid, please try again.");
-		     }
-		 }
-		 System.out.println("Please input your name: ");
-		 name = inputScanner.nextLine();
+
+		while (true) {
+			System.out.println("Please input your ID (numbers only): ");
+			try {
+				id = Long.parseLong(inputScanner.nextLine());
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Your input is invalid, please try again.");
+			}
+		}
+		System.out.println("Please input your name: ");
+		name = inputScanner.nextLine();
 	}
-	
+
 	public void run() {
 		GuitarPlayer player = new GuitarPlayer();
 		Random randomColor = new Random();
 		Random randomBrand = new Random();
-		
+
 		player.setColor(Color.values()[randomColor.nextInt(Color.values().length)]);
 		player.setBrand(GuitarBrand.values()[randomBrand.nextInt(GuitarBrand.values().length)]);
-		
+
 		color = Color.values()[randomColor.nextInt(Color.values().length)];
 		brand = GuitarBrand.values()[randomBrand.nextInt(GuitarBrand.values().length)];
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("Choose a music by input the index of the music");
 			System.out.println(" 1. Secret Base \n 2. Miracle Mountain");
 			System.out.println("or input [back] to back to the Main Menu");
 			playerInput = inputScanner.nextLine();
-			
-			if(playerInput.equals("1")){
+
+			if (playerInput.equals("1")) {
 				m1++;
+
+				//playMusic(m2Location);
+
 				System.out.println("Play \"Secret Base\"");
-			}else if(playerInput.equals("2")){
+
+			} else if (playerInput.equals("2")) {
 				m2++;
 				System.out.println("Play \"Miracle Mountain\"");
-			}else if(playerInput.equals("back")){
+			} else if (playerInput.equals("back")) {
 				break;
-			}
-			else{
+			} else {
 				System.out.println("I don't understand");
 			}
 		}
 	}
-	
+
 	public void end() {
 		System.out.println("Finish!");
+	}
+
+	public static void playMusic(String location) {
+		
+		try {
+			File musicPath = new File(location);
+			if(musicPath.exists()){
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				// Clip clip = AudioSystem.getClip();
+				// clip.open(audioInput);
+				// clip.start();
+				// clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+				AudioFormat format = audioInput.getFormat();
+				DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+				Clip audioClip = (Clip)AudioSystem.getLine(info);
+
+				audioClip.open(audioInput);
+				audioClip.start();
+
+				JOptionPane.showMessageDialog(null, "Press OK to top player");
+			}
+			else{
+				System.out.println("Can't find file");
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
